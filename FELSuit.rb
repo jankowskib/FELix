c9f5c23831b814bce7db9445e3e01f90ef72496e
Bartosz J <thug1337@gmail.com>
2014-12-18 15:28:29 +0100

    Create seperate class FELSuit for flashing

--- FELSuit.rb
+++ FELSuit.rb
@@ -14,23 +14,33 @@
 #   limitations under the License.
 #
 
-# Contains methods to emulate LiveSuit
 class FELix
+end
+
+# Contains methods to emulate LiveSuit
+class FELSuit < FELix
+
+  # Initialize device and check image file
+  # @param device [LIBUSB::Device] a device
+  # @param file [String] LiveSuit image
+  def initialize(device, file)
+    super(device)
+    @image = file
+    raise FELError, "Image not found!" unless File.exist?(@image)
+    @encrypted = encrypted?
+    @structure = fetch_image_structure
+    puts @structure.inspect
+  end
 
   # Flash image to the device
-  # @param file [String] filename
   # @raise error string if something wrong happen
-  def flash(file)
-    # 1. Check image file
-
-    # 2. Let's check device mode
+  def flash
+    # 1. Let's check device mode
     info = get_device_info
-    raise "Failed to get device info. Try to reboot!" unless info
-    # 3. If we're in FEL mode we must firstly boot2fes
-    boot_to_fes if info.mode == :fel
-    info = get_device_info
-    raise "Failed to boot to fes" unless info.mode == :fes
-
+    raise FELError, "Failed to get device info. Try to reboot!" unless info
+    # 2. If we're in FEL mode we must firstly boot2fes
+    boot_to_fes && info = get_device_info if info.mode == :fel
+    raise FELError, "Failed to boot to fes" unless info.mode == :fes
   end
 
   # Download egon, uboot and run code in hope we boot to fes
@@ -39,7 +49,7 @@ class FELix
   # @todo Verify header (eGON.BT0, uboot)
   # @raise [String] error name
   def boot_to_fes(egon, uboot)
-    raise "eGON is too big (#{egon.bytesize}>16384)" if egon.bytesize>16384
+    raise FELError, "eGON is too big (#{egon.bytesize}>16384)" if egon.bytesize>16384
     write(0x2000, egon)
     run(0x2000)
     write(0x4a000000, uboot)
@@ -55,8 +65,8 @@ class FELix
   # @note Use only in :fes mode
   # @note **Warining**: Device may do format anyway if NAND version doesn't match!
   def write_mbr(mbr, format=false)
-    raise "MBR is empty!" if mbr.empty?
-    raise "MBR is too small" unless mbr.bytesize == 65536
+    raise FELError, "MBR is empty!" if mbr.empty?
+    raise FELError, "MBR is too small" unless mbr.bytesize == 65536
     # 1. Force platform->erase_flag => 1 or 0 if we dont wanna erase
     write(0, format ? "\1\0\0\0" : "\0\0\0\0", [:erase, :finish], :fes)
     # 2. Verify status (actually this is unecessary step [last_err is not set at all])
@@ -67,11 +77,42 @@ class FELix
     verify_status(:mbr)
   end
 
-  # Read item from LiveSuit image
-  # @param file [String] filename
-  # @param item [String] item name without path (i.e. system.fex, u-boot.fex,...)
-  def get_image_item(file, item)
+  # Check if image is encrypted
+  # @raise [FELError] if image decryption failed
+  def encrypted?
+    img = File.read(@image, 16) # Read block
+    return false if img.byteslice(0, 8) == "IMAGEWTY"
+    img = FELHelpers.decrypt(img, FELIX_HEADER_KEY) if img.byteslice(0, 8) !=
+    "IMAGEWTY"
+    return true if img.byteslice(0, 8) == "IMAGEWTY"
+    raise FELError, "Failed to decrypt image"
+  end
 
+  # Read item from LiveSuit image
+  # @param item [String] item name without path (i.e. system.fex, u-boot.fex,...)
+  def get_image_item(item)
+
+  end
+
+  # Read header & image items information
+  # @return [AWImage] LiveSuit image structure
+  def fetch_image_structure
+    if @encrypted
+        File.open(@image) do |f|
+        # @todo improve that
+        header = f.read(1024)
+        header = FELHelpers.decrypt(header, FELIX_HEADER_KEY)
+        img_version = header[8, 4].unpack("V").first
+        item_count = header[img_version == 0x100 ? 0x38 : 0x3C, 4].
+          unpack("V").first
+        items = f.read(item_count * 1024)
+        header << FELHelpers.decrypt(items, FELIX_ITEM_KEY)
+        AWImage.read(header)
+    end
+    else
+      # much faster if image is not encrypted
+      File.open(@image) { |f| AWImage.binread(f) }
+    end
   end
 
 end
