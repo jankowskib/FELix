3526decfe542a7a01bd52b3b60ee00926b32599d
Bartosz J <thug1337@gmail.com>
2015-01-26 22:48:05 +0100

    BinData::Record: Fix pp for BinData::Array

--- FELStructs.rb
+++ FELStructs.rb
@@ -28,7 +28,7 @@ class BinData::Record
   def pp
     self.each_pair do |k ,v|
       print "  #{k}".yellow.ljust(40)
-      if v.instance_of? BinData::String
+      if v.instance_of?(BinData::String) || v.instance_of?(BinData::Array)
         puts v.inspect
       else
         puts "0x%08x" % v
